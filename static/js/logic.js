dbee450aab26e549b7ada244531f4f175ab4acb7
joeldemontigny <joeldemontigny@yahoo.ca>
2023-07-23 19:28:06 -0400

    progress

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,5 @@
 // Store our API endpoint as queryUrl.
-let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
 
 // Perform a GET request to the query URL
 d3.json(queryUrl).then(function(data) {
